dac05c4b38beb9bf59a89e61183c6b77d57240a3
Michael Elias <ev3501@wayne.edu>
2018-01-24 23:23:45 -0500

    Integrated db and displayed results from table into HTML page

--- CourseGuru/CourseGuru_App/views.py
+++ CourseGuru/CourseGuru_App/views.py
@@ -3,6 +3,8 @@ from django.template import loader
 from django.shortcuts import render
 import json
 from urllib.request import urlopen
+import psycopg2
+
 
 
 
@@ -10,7 +12,12 @@ from urllib.request import urlopen
 
 # Create your views here.
 def index(request):
-    return render(request, 'CourseGuru_App/index.html', {})
+    myConnection = psycopg2.connect( host='localhost', user='postgres', password='postgres', dbname='CourseGuruDB')
+    cur = myConnection.cursor()
+    cur.execute("SELECT name from mikeTesting")
+    for name in cur.fetchall():
+        prt = name
+    return render(request, 'CourseGuru_App/index.html', {'content': [prt]})
 #    template = loader.get_template('courseguru_app/index.html')
 #    return HttpResponse(template)
 #    return HttpResponse("We made it fam!")
