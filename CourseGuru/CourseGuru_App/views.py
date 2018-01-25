f71b50b38b3b6c595cc608f73efa0f0e5f9faed6
Irarsalan <insia.ahsan@gmail.com>
2018-01-25 14:40:28 -0500

    Add field to enter new question and display contents of db

--- CourseGuru/CourseGuru_App/views.py
+++ CourseGuru/CourseGuru_App/views.py
@@ -3,20 +3,27 @@ from django.template import loader
 from django.shortcuts import render
 import json
 from urllib.request import urlopen
-
-
+import psycopg2
+from django.views.generic import TemplateView, ListView
 
 
 
 # Create your views here.
 def index(request):
-    return render(request, 'CourseGuru_App/index.html', {})
+    myConnection = psycopg2.connect(host = 'Localhost', user = 'postgres', password ='postgres', dbname = 'CourseGuruDB')
+    cur = myConnection.cursor()
+    cur.execute("Select name from Test")
+    #for name in cur.fetchall():
+       ## prt = name
+    prt = [row[0] for row in cur.fetchall()]
+    myConnection.close()
+    return render(request, 'CourseGuru_App/index.html',{'content': prt})
 #    template = loader.get_template('courseguru_app/index.html')
 #    return HttpResponse(template)
 #    return HttpResponse("We made it fam!")
 
-def contact(request):
-    return render(request, 'CourseGuru_App/index.html', {'content':['Hi','Mike']})
+def QAP(request):
+    return render(request, 'CourseGuru_App/QAP.html')
 
 
 #    url = (urlopen('https://canvas.wayne.edu/api/v1/courses').read()
