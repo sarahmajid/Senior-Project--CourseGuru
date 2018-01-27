23d18e8a6fe7c77f72834c821bbbbe6f11eb6574
Andriy Marynovskyy <andriym2010@gmail.com>
2018-01-27 02:31:11 -0500

    trying update all files to current master files

--- CourseGuru/CourseGuru/settings.py
+++ CourseGuru/CourseGuru/settings.py
@@ -77,10 +77,10 @@ WSGI_APPLICATION = 'CourseGuru.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'CourseGuruDB',
-        'USER':'postgres',
-        'PASSWORD': 'postgres',
-        'HOST': '',
+        'NAME': 'postgres',
+        'USER':'cguser',
+        'PASSWORD': 'csc4996!',
+        'HOST': 'aa1kaxr8yrczw6m.cynst32f7ubm.us-east-2.rds.amazonaws.com',
         'PORT': '5432',
     }
 }
