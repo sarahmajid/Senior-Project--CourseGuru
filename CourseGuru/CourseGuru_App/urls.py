6b6683164f949b032805a8f69a91ecde837c93cd
Andriy Marynovskyy <andriym2010@gmail.com>
2018-01-27 02:07:18 -0500

    Revert "Merge branch 'MikeBranch' into AndriysBranch"
    This reverts commit a7cb56cc1d83e872f1cd82c432547602cc3595e6, reversing
    changes made to 902f94253729276c3fc42d73d2447f9ca6c19d1a.

--- CourseGuru/CourseGuru_App/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
- #   path('contact/', views.contact, name='contact')
-    path('', views.index, name='index'),
-    path('answer/', views.answer, name='answer'),
-    
-]
