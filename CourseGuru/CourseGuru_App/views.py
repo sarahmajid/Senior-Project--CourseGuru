8aca6675a4be25780d9eaa60c2c2eeebf1667348
Michael Elias <ev3501@wayne.edu>
2018-02-02 16:45:16 -0500

    Application now reads in a LUIS intent when a question is asked.

--- CourseGuru/CourseGuru_App/views.py
+++ CourseGuru/CourseGuru_App/views.py
@@ -6,6 +6,10 @@ from urllib.request import urlopen
 import psycopg2
 from django.http.response import HttpResponseRedirect
 
+#testing forum bot
+import requests
+import json
+
 #importing models 
 from CourseGuru_App.models import user
 from CourseGuru_App.models import course
@@ -19,7 +23,19 @@ def index(request):
     if request.method == "POST":
         nq = request.POST.get('NQ')
         questions.objects.create(question = nq, course_id = 1, user_id = 1)
-        return HttpResponseRedirect('/')
+        intent = cbAnswer(nq)
+        
+        questions.objects.filter(id=4).update(question=intent)
+        #=======================================================================
+        # try:
+        #     r = request.get('https://westus.api.cognitive.microsoft.com/luis/v2.0/apps/ee579764-9ebf-44fd-a904-e3353a691b7e?subscription-key=a0d35b39c3874ec1b05ff5bececfa7eb&verbose=true&timezoneOffset=0&q=%s' % nq, '')
+        #     luisStr = json.loads(r.text)
+        #     luisStr = luisStr['topScoringIntent']['intent']
+        #     questions.objects.filter(id=3).update(question=luisStr)
+        # except Exception as e:
+        #     questions.objects.filter(id=3).update(question='failed')
+        #=======================================================================
+        return HttpResponseRedirect('/') 
     qData = questions.objects.all()
     return render(request, 'CourseGuru_App/index.html', {'content': qData})
 
@@ -33,12 +49,16 @@ def answer(request):
         return HttpResponseRedirect('/answer/?id=%s' % qid)
     aData = answers.objects.filter(question_id = qid)
     qData = questions.objects.get(id = qid)
-
     return render(request, 'CourseGuru_App/answer.html', {'answers': aData, 'Title': qData})
 
 def chatbot(request):
     return render(request, 'CourseGuru_App/botchat.html',)
 
+def cbAnswer(nq):
+    r = requests.get('LUISENDPOINTq=%s' % nq)
+    luisStr = json.loads(r.text)
+    intent = luisStr['topScoringIntent']['intent']
+    return(intent)
 
 #    ---Canvas code---
 #    url = (urlopen('https://canvas.wayne.edu/api/v1/courses').read()
