a7508f97dab48778f60c01030da6c7291e620a99
Scott Denomme <sdenomme@wayne.edu>
2018-02-02 21:55:56 -0500

    Fixed settings

--- CourseGuru/CourseGuru/settings.py
+++ CourseGuru/CourseGuru/settings.py
@@ -78,12 +78,12 @@ WSGI_APPLICATION = 'CourseGuru.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'AmesiteDB',                      
-        'USER': 'cguser',
-        'PASSWORD': 'PlsDontCommitThis1!',
-        'HOST': '18.218.213.31',
-        'PORT': '5432',
+        'ENGINE': '',
+        'NAME': '',                      
+        'USER': '',
+        'PASSWORD': '',
+        'HOST': '',
+        'PORT': '',
     }
 }
 
