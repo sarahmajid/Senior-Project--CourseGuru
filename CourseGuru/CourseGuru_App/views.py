9841308b29d02577eafcdc59b5124cecc6c5a1d7
Andriy Marynovskyy <andriym2010@gmail.com>
2018-02-07 23:30:07 -0500

    working on integrating entities from LUIS

--- CourseGuru/CourseGuru_App/views.py
+++ CourseGuru/CourseGuru_App/views.py
@@ -252,7 +252,7 @@ def chatbot(request):
     return render(request, 'CourseGuru_App/botchat.html',)
 
 def cbAnswer(nq):
-    r = requests.get('https://westus.api.cognitive.microsoft.com/luis/v2.0/apps/6059c365-d88a-412b-8f33-d7393ba3bf9f?subscription-key=c574439a46e64d8cb597879499ccf8f9&verbose=true&timezoneOffset=0&q=%s' % nq)
+    r = requests.get('%s' % nq)
     luisStr = json.loads(r.text)
     #Grabs intent score of question
     luisScore = float(luisStr['topScoringIntent']['score'])
