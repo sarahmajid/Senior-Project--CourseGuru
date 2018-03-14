ff39ff3ab52fa86b4d9178d04dda05c1954b3a36
Andriy Marynovskyy <andriym2010@gmail.com>
2018-03-14 00:01:22 -0400

    small improvements to pdfParser.

--- CourseGuru/CourseGuru_App/pdfParser.py
+++ CourseGuru/CourseGuru_App/pdfParser.py
@@ -192,7 +192,6 @@ def pullInfo(file):
 #===============================================================================
 
 def joinKeyWords(parsedFile, keyWords):
-    
     i=0
     while i<len(parsedFile)-4:
         for n in keyWords:
