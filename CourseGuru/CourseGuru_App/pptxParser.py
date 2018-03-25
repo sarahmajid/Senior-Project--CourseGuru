f3ec60722b498281e73979ba9fdefef39dd27903
Andriy Marynovskyy <andriym2010@gmail.com>
2018-03-25 15:50:14 -0400

    finished Initial parsing algorithm for pptx files.

--- CourseGuru/CourseGuru_App/pptxParser.py
+++ CourseGuru/CourseGuru_App/pptxParser.py
@@ -25,9 +25,9 @@ def restructForDB (text, cid, catID, fileid):
     botSearch = ' '.join(botSearch)
     botSearch = botSearch.lower()
     
-    #botanswers.objects.create(answer = text, rating = 0, category_id = catID.id, entities = botSearch, course_id = cid, file_id = fileid)
+    botanswers.objects.create(answer = text, rating = 0, category_id = catID.id, entities = botSearch, course_id = cid, file_id = fileid)
 #testing PRP    
-    print(botSearch)
+#    print(botSearch)
 
 
 def parsePPTX(file, cid, catID, fileid):
@@ -38,6 +38,7 @@ def parsePPTX(file, cid, catID, fileid):
     for slide in parseFile.slides:
         title= slide.shapes.title.text_frame.text
         dataArray.append(title+'<br>')
+#testing PRP   
 #        print(title + '<br>'+"=============")
         for shape in slide.shapes:
             if shape.has_text_frame:
@@ -48,6 +49,7 @@ def parsePPTX(file, cid, catID, fileid):
                             dataArray[-1] = dataArray[-1] + '' + (text)
                         else: 
                             dataArray[-1] = dataArray[-1] + ' ' + (text) + '<br>'
+#testing PRP   
 #                            print(text)
             elif shape.has_table:
                 table = shape.table
@@ -77,4 +79,5 @@ def parsePPTX(file, cid, catID, fileid):
     file.close()
     for n in dataArray:
         restructForDB(n, cid, catID, fileid)
-        print(n)
+#testing PRP   
+#        print(n)
