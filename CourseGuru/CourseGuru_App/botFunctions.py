b52b9f07f62d6a41859b78665bb8f2c6a7b101c1
Mike28e <ev3501@wayne.edu>
2018-03-28 20:59:25 -0400

    Modifications to chatbot answering function.. Preventing submitting twice by double clicking button..

--- CourseGuru/CourseGuru_App/botFunctions.py
+++ CourseGuru/CourseGuru_App/botFunctions.py
@@ -4,12 +4,15 @@ from CourseGuru_App.models import category, botanswers
 from nltk.corpus import stopwords
 from nltk.tokenize.moses import MosesDetokenizer
 
+import spacy
+from nltk.corpus import wordnet
+
 
 def cbAnswer(nq, courseID=0, chatWindow=False):
     r = requests.get('https://westus.api.cognitive.microsoft.com/luis/v2.0/apps/6059c365-d88a-412b-8f33-d7393ba3bf9f?subscription-key=c574439a46e64d8cb597879499ccf8f9&verbose=true&timezoneOffset=-300&q=%s' % nq)
     luisStr = json.loads(r.text)
     #Grabs intent score of question
-    luisScore = float(luisStr['topScoringIntent']['score'])
+    ####luisScore = float(luisStr['topScoringIntent']['score'])
     #Grabs intent of question
     luisIntent = luisStr['topScoringIntent']['intent']
     #Grabs entities
@@ -17,58 +20,70 @@ def cbAnswer(nq, courseID=0, chatWindow=False):
         return('Hello, how can I help you?')
     elif luisIntent == 'Greetings':
         return
-    #If intent receives a lower score than 75% or there is no intent, the question does not get answered
+    #If there is no intent, the question does not get answered
     elif luisIntent == "None" and chatWindow == True:
         return("Sorry, I didn't understand that.")
     elif luisIntent == "None":
         return
     
-    #teachLuis(nq, 'Other')
+    #API for synonyms
+    #s = requests.get('http://words.bighugelabs.com/api/2/267f9470ea934d007fe08bc9edc73c6b/book/json')
+    #for a in synStr['noun']['syn']:
+    #    print(a)
+
     
-    luisEntities = ""
-    
+    luisEntities = []
+    nlp = spacy.load('en')
     #regex = re.compile('[^a-zA-Z]')
-    if luisIntent != 'Other':
-        z = 0
-        for x in luisStr['entities']:
-            newEnt = luisStr['entities'][z]['entity']
-            #newEnt = regex.sub('', newEnt)
-            if z == 0:
-                luisEntities += newEnt
-            else:
-                luisEntities += ',' + newEnt
-            z += 1
-    else: 
-        #Manual stripping question for entities
-        detokenizer = MosesDetokenizer()
-        ent_list = nltk.word_tokenize(nq)
-        ent_list = [word for word in ent_list if word not in stopwords.words('english')]
-        detokenizer.detokenize(ent_list, return_str=True)
-        ent_str = " ".join(ent_list).lower()
-        #Removes punctuation from string
-        regex = re.compile('[%s]' % re.escape(string.punctuation))
-        ent_str = regex.sub('', ent_str)
-        luisEntities = re.sub("\s+", ",", ent_str.strip())   
+#     if luisIntent != 'Other':
+#         z = 0
+#         for x in luisStr['entities']:
+#             newEnt = luisStr['entities'][z]['entity']
+#             luisEntities.append(newEnt)
+#             z += 1
+#     else: 
+    #Manual stripping question for entities
+    detokenizer = MosesDetokenizer()
+    ent_list = nltk.word_tokenize(nq)
+    ent_list = [word for word in ent_list if word not in stopwords.words('english')]
+    detokenizer.detokenize(ent_list, return_str=True)
+    #ent_str = " ".join(ent_list).lower()
+    #Removes punctuation from string
+    regex = re.compile('[%s]' % re.escape(string.punctuation))
+    for ind,ent in enumerate(ent_list):
+        doc = nlp(ent)
+        entTemp = regex.sub('', doc[0].lemma_)
+        if entTemp != '':
+            luisEntities.append(entTemp)
+        
+    luisEntities = [word for word in luisEntities if word not in stopwords.words('english')]
     
-    if luisEntities == "" and chatWindow == True:
+    #ent_str = regex.sub('', ent_str)
+    #tempStr = re.sub("\s+", ",", ent_str.strip())   
+    #luisEntities = tempStr.split(',')
+    
+    if luisEntities == [] and chatWindow == True:
         return("Sorry, I didn't understand that.")
-    elif luisEntities == "":
+    elif luisEntities == []:
         return
     
-    #Add variations of names a student would call the teacher if one is found
+    for ent in luisEntities:
+        print(ent)
+             
+    #Add all synonyms of teacher if one is found
     profNames = ['instructor','teacher','professor']
     for name in profNames:
-        if name in luisEntities.lower():
-            for addName in profNames:
-                if addName not in luisEntities.lower():
-                    luisEntities += ',' + addName
-        else:
-            continue
-        break
-    if 'ta' in luisEntities.lower():
-        luisEntities += ',' + 'teaching assistant'
-                    
-    
+        for ent in luisEntities:
+            if name == ent.lower():
+                for addName in profNames:
+                    if addName not in luisEntities:
+                        luisEntities.append(addName)
+            else:
+                continue
+            break
+    if 'ta' in luisEntities:
+        luisEntities.append('teaching assistant')
+                     
     entAnswer = getIntentAns(luisIntent, luisEntities, courseID, chatWindow)
     if entAnswer == "":
         return
@@ -77,22 +92,11 @@ def cbAnswer(nq, courseID=0, chatWindow=False):
     return(botAns)
 
 # returns a good match to entities answer object  
-def getIntentAns(luisIntent, luisEntities, courseID=0, chatWindow=False):    
+def getIntentAns(luisIntent, entitiesList, courseID=0, chatWindow=False):    
     count = 0
     answr = ""
-    
-    entitiesList = luisEntities.lower().split(",")
+
     catgry = category.objects.get(intent = luisIntent)
-    
-    #Changes plural to singular
-    #lem = WordNetLemmatizer()
-    #for ind, ent in enumerate(entitiesList):
-    #    entitiesList[ind] = lem.lemmatize(ent)
-    
-    #Temporary until lemmatizer is fixed
-    for ind, ent in enumerate(entitiesList):
-        if ent[-1:].lower() == 's':
-            entitiesList[ind] = ent[:-1]
     
     if courseID != 0:
         filtAns = botanswers.objects.filter(category_id = catgry.id, course_id = courseID)
